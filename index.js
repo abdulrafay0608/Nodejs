065866f2eb2ec9a2020cb7ef768b1b462e85bbec
Abdul Rafay <abdulrafay0608@gmail.com>
2023-10-29 03:30:06 +0500

    connected mongoose
    Connected mongoose With Node js and Express js

--- index.js
+++ index.js
@@ -2,20 +2,29 @@ const express = require('express')
 const app = express()
 const port = 3000
 const router = require("./router/user")
-app.use('/user' , router)    
+const mongoose = require('mongoose');
+app.use('/user', router)
+
+
+mongoose.connect('mongodb+srv://abdulrafay0608:abdulrafay@backend.bcab9hf.mongodb.net/').then(() => {
+    console.log('Mongodb Connected')
+}).catch((err) => console.log(err))
+
+
+
 app.get('/', (req, res) => {
     res.send({
-        status : 200,
+        status: 200,
         Message: "Project is working...."
     })
 })
 app.post('/', (req, res) => {
     res.send({
-        status : 200,
+        status: 200,
         Message: "api is working...."
     })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
--- router/user.js
+++ router/user.js
@@ -1,19 +1,20 @@
 const express = require('express')
 const router = express.Router()
 const adduser = require("./adduser")
-
 router.use('/adduser', adduser)
-const user = [
-    { name: "Alice", email: "alice@gmail.com", id: 1 },
-    { name: "Bob", email: "bob@gmail.com", id: 2 },
-    { name: "Charlie", email: "charlie@gmail.com", id: 3 },
-    { name: "David", email: "david@gmail.com", id: 4 },
-    { name: "Ella", email: "ella@gmail.com", id: 5 },
-    { name: "Frank", email: "frank@gmail.com", id: 6 },
-    { name: "Grace", email: "grace@gmail.com", id: 7 },
-    { name: "Henry", email: "henry@gmail.com", id: 8 },
-    { name: "Ivy", email: "ivy@gmail.com", id: 9 }
-  ];
+const userModel = require("../model/user")
+console.log(userModel);
+// const user = [
+//     { name: "Alice", email: "alice@gmail.com", id: 1 },
+//     { name: "Bob", email: "bob@gmail.com", id: 2 },
+//     { name: "Charlie", email: "charlie@gmail.com", id: 3 },
+//     { name: "David", email: "david@gmail.com", id: 4 },
+//     { name: "Ella", email: "ella@gmail.com", id: 5 },
+//     { name: "Frank", email: "frank@gmail.com", id: 6 },
+//     { name: "Grace", email: "grace@gmail.com", id: 7 },
+//     { name: "Henry", email: "henry@gmail.com", id: 8 },
+//     { name: "Ivy", email: "ivy@gmail.com", id: 9 }
+//   ];
   
   
 
